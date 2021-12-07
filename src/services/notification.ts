b141006bc0acdf096b5210008281df6a131b1d16
ikhsangama <ikhsangama@gmail.com>
2021-12-07 07:21:00 +0700

    secret key nt found handler

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -54,6 +54,7 @@ export class NotificationService {
 
         const businessSetting = await this.businessRepository.findBusinessSettingByUuid(notification.business_setting_uuid);
         if (!businessSetting) throw {name: 'Not Found', msg: 'business setting not found', status: 404};
+        if (!businessSetting.secret_key) throw {name: 'Not Found', msg: 'secret key header not found', status: 404};
         let notificationResponse = await this.notificationRepository.saveNotification({ ...notification,
             is_sent: false,
             retry_count: notification.retry_count || 0,
@@ -64,7 +65,8 @@ export class NotificationService {
             method: "POST",
             url: businessSetting.webhook_url,
             headers: {
-                "payment-key": notificationResponse.payment_uuid
+                "impd-key": notificationResponse.payment_uuid,
+                "secret-key": businessSetting.secret_key
             },
             data: notificationResponse.payload
         })
